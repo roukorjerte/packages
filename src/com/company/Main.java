9a1011eea48bddda1103e732039414bb003060ef
roukorjerte <roukorjerte@gmail.com>
2021-07-07 20:15:20 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import Forest.Something.SomeClass;
+import Forest.Squirell;
+import Forest.Tree;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Tree tree1 = new Tree();
+        Squirell squirell1 = new Squirell();
+    }
+    SomeClass newItem1 = new SomeClass();
+}
